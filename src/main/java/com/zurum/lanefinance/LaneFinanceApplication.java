9e0d61fc65b86fa75b73b40ab44ce21cdc70573d
zurum <zurumogbonda@gmail.com>
2022-07-05 23:17:48 +0100

    initial commit

---
+++ src/main/java/com/zurum/lanefinance/LaneFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.zurum.lanefinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaneFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaneFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zurum/lanefinance/LaneFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zurum.lanefinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaneFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
