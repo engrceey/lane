321a3b83fbccf9f16ed4a1b8426943fd0b95c694
zurum <zurumogbonda@gmail.com>
2022-07-06 13:42:00 +0100

    add app util

---
+++ src/main/java/com/zurum/lanefinance/utils/AppUtil.java
@@ -0,0 +1,56 @@
+package com.zurum.lanefinance.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+@Slf4j
+public class AppUtil {
+    private static ObjectMapper mapper() {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Jdk8Module());
+        return mapper;
+    }
+
+    public static String toJson(Type type) {
+        try {
+            return mapper().writeValueAsString(type);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error occurred serializing object to json string, error => " + e.getMessage());
+        }
+    }
+
+    public static <T> String toJson(T t) {
+        try {
+            return mapper().writeValueAsString(t);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error occurred serializing object to json string, error => " + e.getMessage());
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return mapper().readValue(json, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Error occurred deserializing object to json string: " + e.getMessage());
+        }
+    }
+
+//    public static String getPrincipal() {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (authentication == null || !authentication.isAuthenticated()) {
+//            return "system";
+//        }
+//        return authentication.getName();
+//    }
+
+
+}
