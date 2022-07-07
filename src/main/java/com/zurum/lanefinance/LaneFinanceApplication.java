e29acc204610512118571937a9973a413283f8e9
zurum <zurumogbonda@gmail.com>
2022-07-07 02:58:02 +0100

    update test properties

--- src/main/java/com/zurum/lanefinance/LaneFinanceApplication.java
+++ src/main/java/com/zurum/lanefinance/LaneFinanceApplication.java
@@ -2,11 +2,12 @@ package com.zurum.lanefinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-
+@EnableAsync
 @SpringBootApplication
 public class LaneFinanceApplication {
 
