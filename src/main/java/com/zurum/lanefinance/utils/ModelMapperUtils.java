63b78d29fa3d30a47925afda7bf9a64cc9dfed38
zurum <zurumogbonda@gmail.com>
2022-07-07 01:40:41 +0100

    add password validation util

---
+++ src/main/java/com/zurum/lanefinance/utils/ModelMapperUtils.java
@@ -0,0 +1,45 @@
+package com.zurum.lanefinance.utils;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.data.domain.Page;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class ModelMapperUtils {
+
+    private static final ModelMapper modelMapper;
+
+    static {
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+    private ModelMapperUtils() {
+    }
+
+    public static <S, D> D map(final S entity, Class<D> outClazz) {
+        return modelMapper.map(entity, outClazz);
+    }
+
+    public static <S, D> List<D> mapAll(final Collection<S> entityList, Class<D> outClazz) {
+        return entityList.stream().map(entity -> map(entity, outClazz)).collect(Collectors.toList());
+    }
+
+    public static <S, D> List<D> mapAll(final List<S> entityList, Class<D> outClazz) {
+        return entityList.stream().map(entity -> map(entity, outClazz)).collect(Collectors.toList());
+    }
+
+    public static <S, D> List<D> mapAll(final Page<S> entityList, Class<D> outClazz) {
+        return entityList.stream().map(entity -> map(entity, outClazz)).collect(Collectors.toList());
+    }
+
+
+    public static <S, D> D map(final S source, D destination) {
+        modelMapper.map(source, destination);
+        return destination;
+    }
+}
---
+++ src/main/java/com/zurum/lanefinance/utils/PasswordMatch.java
@@ -0,0 +1,17 @@
+package com.zurum.lanefinance.utils;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy=PasswordMatchValidator.class)
+public @interface PasswordMatch {
+    String message() default "Password and Confirm Password do not match.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
