bddce2ec0d51aeeda02814eb5b4539d9d8a873db
zurum <zurumogbonda@gmail.com>
2022-07-07 01:40:00 +0100

    configs/properties

---
+++ src/main/java/com/zurum/lanefinance/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.zurum.lanefinance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+}
---
+++ src/main/java/com/zurum/lanefinance/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.zurum.lanefinance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    //Meta data for swagger
+    private ApiInfo metaInfo()
+    {
+        return new ApiInfo(
+                "Lane Financial Application API",
+                "This Application manages registrations and transactions",
+                "1.0",
+                "Terms of Server",
+                new Contact("My LinkedIn Account", "https://www.linkedin.com/in/zurum-ogbonda/", "ogbondachristian@gmail.com"),
+                "Apache License Version 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0.html",
+                Collections.emptyList()
+        );
+    }
+
+}
