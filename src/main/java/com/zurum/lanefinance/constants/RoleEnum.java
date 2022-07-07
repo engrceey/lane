f8bc0e00dfedfc1a1e631318c7cee8286564bc56
zurum <zurumogbonda@gmail.com>
2022-07-07 01:41:48 +0100

    update models

--- src/main/java/com/zurum/lanefinance/constants/RoleEnum.java
+++ src/main/java/com/zurum/lanefinance/constants/RoleEnum.java
@@ -1,8 +1,13 @@
 package com.zurum.lanefinance.constants;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum RoleEnum {
-    USER, ADMIN, ACCOUNT_OFFICER
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN"), ACCOUNT_OFFICER("ROLE_ACCOUNT_OFFICER");
+
+    private final String role;
+
 }
--- src/main/java/com/zurum/lanefinance/repository/VerificationTokenRepository.java
+++ src/main/java/com/zurum/lanefinance/repository/VerificationTokenRepository.java
@@ -3,5 +3,5 @@ package com.zurum.lanefinance.repository;
 import com.zurum.lanefinance.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, String> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
