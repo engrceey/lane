9ee0973ee26e53a5be2a9b327004de399a2f1e14
zurum <zurumogbonda@gmail.com>
2022-07-10 05:25:10 +0100

    remove

--- src/main/java/com/zurum/lanefinance/service/AuthService.java
+++
@@ -1,4 +0,0 @@
-package com.zurum.lanefinance.service;
-
-public interface AuthService {
-}
--- src/main/java/com/zurum/lanefinance/service/implementations/AuthServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.zurum.lanefinance.service.implementations;
-
-public class AuthServiceImpl {
-}
